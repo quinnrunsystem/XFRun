f65edd640141d172211e804c875b52fc31f71cdd
khoant <sneijdernguyen@gmail.com>
2018-10-15 18:01:43 +0700

    no message

---
+++ XFRun.UI/FlexLayoutExtend.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using Xamarin.Forms;
+
+namespace XFRun.UI.Forms
+{
+    public class FlexLayoutExtend : FlexLayout
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            nameof(ItemsSource),
+            typeof(IEnumerable),
+            typeof(FlexLayoutExtend),
+            propertyChanged: OnItemsSourceChanged);
+
+        static void OnItemsSourceChanged(BindableObject bindable, object oldVal, object newVal)
+        {
+            IEnumerable newValue = newVal as IEnumerable;
+            var layout = (FlexLayoutExtend)bindable;
+
+            layout.Children.Clear();
+            if (newValue != null)
+            {
+                foreach (var item in newValue)
+                {
+                    layout.Children.Add(layout.CreateChildView(item));
+                }
+            }
+        }
+
+        View CreateChildView(object item)
+        {
+            ItemTemplate.SetValue(BindableObject.BindingContextProperty, item);
+            return (View)ItemTemplate.CreateContent();
+        }
+
+
+        public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
+                nameof(ItemTemplate),
+                typeof(DataTemplate),
+                typeof(FlexLayoutExtend));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+    }
+}
+
---
+++ XFRun.UI/PinView.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XFRun.UI.Forms.Renderers;
+
+namespace XFRun.UI.Forms
+{
+    public partial class PinView : RoundedCornerView
+    {
+        private List<Label> _labels;
+        public string PinCode = String.Empty;
+        public static BindableProperty CountProperty = BindableProperty.Create(nameof(CountProperty), typeof(int), typeof(PinView), 4, propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            PinView pinView = (PinView)bindable;
+            pinView.CreateListTextbox((int)newValue);
+        });
+
+        public int Count
+        {
+            get
+            {
+                return (int)GetValue(CountProperty);
+            }
+            set
+            {
+                SetValue(CountProperty, value);
+            }
+        }
+
+        private void CreateListTextbox(int count)
+        {
+            _labels = new List<Label>();
+            for (int i = 0; i < count; i++)
+            {
+                ExtendedFrame frame = new ExtendedFrame()
+                {
+                    BorderThickness = 1,
+                    CornerRadius = 15,
+                    BorderColor = Color.Gray,
+                    HeightRequest = 50,
+                    WidthRequest = 50
+                };
+
+                Label label = new Label()
+                {
+                    TextColor = Color.Accent,
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center
+                };
+                frame.Content = label;
+                _labels.Add(label);
+                flexLayout.Children.Add(frame);
+            }
+        }
+
+        public PinView()
+        {
+            InitializeComponent();
+            fakeEntry.TextChanged += FakeEntry_TextChanged;
+        }
+
+        void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            fakeEntry.Focus();
+        }
+
+        void FakeEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            foreach (var item in _labels)
+            {
+                item.Text = e.NewTextValue;
+            }
+        }
+
+    }
+}
